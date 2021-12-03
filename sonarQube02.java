75f1f2c5099d52c417b6656a5f82227f80f0f15d
KamiliaBenkirane <95300119+KamiliaBenkirane@users.noreply.github.com>
2021-12-03 15:31:18 +0100

    Update sonarQube02.java

--- sonarQube02.java
+++ sonarQube02.java
@@ -1,4 +1,5 @@
-public class sonarQube02 {
+package org.test2;
+public class sonarQube2 {
 
     String printNumbers(String stringToAdd, int x, int y) {
         return String.format("Numbers are %s %d %d", stringToAdd, x, y);
