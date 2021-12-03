2230f326a0e8331418b54ab69fa72c234db27d21
KamiliaBenkirane <95300119+KamiliaBenkirane@users.noreply.github.com>
2021-12-03 14:47:56 +0100

    Update sonarQube01.java

--- sonarQube01.java
+++ sonarQube01.java
@@ -1,5 +1,5 @@
 public class sonarQube01 {
-    int addHeader(String stringToAdd, int x, int y){
-        return String.format("Added Header %d %d %d", stringToAdd, x, y);
+    String addHeader(String stringToAdd, int x, int y){
+        return int.format("Added Header %d %d %d", stringToAdd, x, y);
     }
 }
