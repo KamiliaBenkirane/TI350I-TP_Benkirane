5787c36f6cec05729b580c6a0dfa5a4dc8fba940
KamiliaBenkirane <95300119+KamiliaBenkirane@users.noreply.github.com>
2021-12-03 15:27:40 +0100

    Update sonarQube04.java

--- sonarQube04.java
+++ sonarQube04.java
@@ -1,6 +1,6 @@
-public class sonarQube04 {
+package org.test4;
+public class sonarQube4 {
     private int x;
-    private int y = 0;
     int getX() { return x;}
     void setX(int val){ x = val; }
 }
